7f4035ced8da0858d00d33df60e368b94b40a0c9
Suprim Shrestha <suprim.shresth@gmail.com>
2024-01-08 14:05:14 +0545

    Add cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 
 import cookieParser from "cookie-parser";
 
@@ -15,6 +16,13 @@ const app = express();
 app.use(cookieParser(config.cookieSecret));
 
 app.use(express.json());
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 app.use(routes);
 
