f91767565c5ffb8e7a097d7a3609ea7f039a8bb7
Suprim Shrestha <suprim.shresth@gmail.com>
2024-01-11 14:14:02 +0545

    Add close all tabs and open tabs function

--- extension/popup/popup.js
+++ extension/popup/popup.js
@@ -1,5 +1,17 @@
-const closeBtn = document.getElementById("closeTabBtn");
+const closeInactiveTabsBtn = document.getElementById("closeInactiveTabsBtn");
 
-closeBtn.addEventListener("click", () => {
+closeInactiveTabsBtn.addEventListener("click", () => {
   chrome.runtime.sendMessage({ action: "closeAllInactiveTabs" });
+});
+
+const closeAllTabsBtn = document.getElementById("closeAllTabsBtn");
+
+closeAllTabsBtn.addEventListener("click", () => {
+  chrome.runtime.sendMessage({ action: "closeAllTabs" });
+});
+
+const openTabsBtn = document.getElementById("openTabsBtn");
+
+openTabsBtn.addEventListener("click", () => {
+  chrome.runtime.sendMessage({ action: "openTabs", groupId: 3 });
 });
